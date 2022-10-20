c12d3010d9b3af22c21224bd1a345a4d9ee52a60
Mengqiao Wang <mwan7932@uni.sydney.edu.au>
2022-10-20 11:28:20 +0800

    Update url

--- Capstone/urls.py
+++ Capstone/urls.py
@@ -7,6 +7,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("login.urls")),
+    path('', include('rest_framework.urls', namespace='rest_framework')),
+    # path("api/", include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
