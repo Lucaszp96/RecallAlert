6ad70741e33b87298f7c14ada594a99170adb15f
Mengqiao Wang <mwan7932@uni.sydney.edu.au>
2022-10-20 11:24:17 +0800

    Modify database name

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'Capstone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 # pip install mysqlclient
